20a68f90c40d6fa9fabce6934be7c24e2980de68
Daniel <danie.falzon6@gmail.com>
2020-10-31 15:51:20 +0100

    Seperate content into files. Added Sidebar + menu

---
+++ themes/wphierarchy/footer.php
@@ -0,0 +1,16 @@
+</div><!-- #content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <a href="<?php echo esc_url(
+                    __('http://wordpress.org/', 'wphierarchy'),
+                    'WordPress'
+                ); ?>">
+    </a>
+</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
