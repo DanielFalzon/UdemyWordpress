602390d18b27b48dae2cdea6c73dcac8d5223dc2
Daniel <danie.falzon6@gmail.com>
2020-11-01 11:12:16 +0100

    Hnadling mime types inidividually

--- themes/wphierarchy/functions.php
+++ themes/wphierarchy/functions.php
@@ -1,4 +1,8 @@
 <?php 
+    @ini_set( 'upload_max_size' , '64M' );
+    @ini_set( 'post_max_size', '64M');
+    @ini_set( 'max_execution_time', '300' );
+
     //Add support to WP features
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
